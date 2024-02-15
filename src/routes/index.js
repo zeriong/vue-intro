1a7de075f078e08ee7baffcfa50e4b1d20092561
QU-08 <jaeryong95@gmail.com>
2024-02-15 10:08:43 +0900

    초기 화면 로그인화면으로 리다이렉트 지정

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,11 @@ Vue.use(VueRouter);
 
 export default new VueRouter({
     routes: [
+        // 초기화면 로그인 화면으로 redirect
+        {
+            path: '/',
+            redirect: '/login'
+        },
         {
             path: '/login',
             // 함수형태로 컴포넌트를 지정하여 코드스플리팅 사용
