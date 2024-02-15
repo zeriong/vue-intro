0efc1a634071345d38b098dfe84499549a0f4def
QU-08 <jaeryong95@gmail.com>
2024-02-15 10:38:02 +0900

    router: history mode를 추가하여 URL파라미터에 # 제거, history mode 설명 주석 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,11 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 export default new VueRouter({
+    // history 모드를 추가하여 url파라미터에 #을 없앨 수 있다.
+    // history 모드를 추가하면 서버에서 해당 url 에 정의된 파일을 알 수 없게 된다.
+    // 그렇기 때문에 배포를 하게되는 경우 Vue Router 도큐먼트에 History Mode 를 검색해서
+    // Server Configuration 을 설정해주어야 한다.
+    mode: 'history',
     routes: [
         // 초기화면 로그인 화면으로 redirect
         {
